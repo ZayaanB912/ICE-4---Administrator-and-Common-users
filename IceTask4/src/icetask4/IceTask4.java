a8818493e57a94da942d68e0cbeee226e933446d
Zayaan Bhikha <Zayaanbhikha@gmail.com>
2026-04-09 20:34:45 +0200

    Added code to Main class
    In this class I have started where the the program will start. I need to create the LoginFrame Class now and code the full program.

--- IceTask4/src/icetask4/IceTask4.java
+++ IceTask4/src/icetask4/IceTask4.java
@@ -5,7 +5,10 @@ public class IceTask4 {
 
 
     public static void main(String[] args) {
-    }
+    javax.swing.SwingUtilities.invokeLater(() -> {
+        new LoginFrame();
+    });
+}
     
     
 }
